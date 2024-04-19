147a4c70515e4c3dd859ef132376320595b5e3fe
DanRuizK57 <d.ruiz03@ufromail.cl>
2024-04-19 16:42:04 -0400

    Limpieza

--- sketch.js
+++ sketch.js
@@ -1,21 +1,14 @@
-//import fetchComments from "./comments";
 let comments;
 let sentiment;
-let commentBodies 
 let prediction;
 let clasifiedComments = [];
 
 async function setup() {
   comments = await fetchComments();
-  //commentBodies = await Comment.commentBodies;
-  
-  console.log(comments);
   sentiment = ml5.sentiment('movieReviews', modelReady);
-  
 }
 
  function modelReady() {
-  
   console.log('Model Loaded!');
   
   // const text = "I love this movie!";
@@ -27,26 +20,25 @@ async function setup() {
   //                   box.style.backgroundColor = '#8BC34A'; // Verde si es positivo
   //               } else if (prediction.score < 0.5) {
   //                   box.style.backgroundColor = '#FF5722'; // Naranja si es negativo
-  //               }
+   //               }
+   
   for (const comment of comments) {
     prediction = sentiment.predict(comment);
     clasifiedComments.push({
       text: comment,
-      prediction: prediction
+      prediction: prediction.score
     });
-    console.log(prediction);
-  }
-  
+   }
+   
+   console.log(clasifiedComments);
 }
 
-  async function fetchComments () {
+async function fetchComments () {
   try {
       const response = await fetch('https://dummyjson.com/comments');
       const data = await response.json();
       comments = data.comments;
-      console.log("paso");
       commentBodies = comments.map(comment => comment.body);
-      console.log(commentBodies);
       return commentBodies;
   } catch (error) {
       console.error('Error fetching data:', error);
