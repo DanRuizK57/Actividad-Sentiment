c4d90acb8c06ab94c02c166eba6fce6925a41ed8
JoakoVillagran <j.villagran04@ufromail.cl>
2024-04-19 13:09:05 -0400

    recuadro con cambio de color

--- sketch.js
+++ sketch.js
@@ -10,4 +10,11 @@ function modelReady() {
   const text = "I love this movie!";
   const prediction = sentiment.predict(text);
   console.log(prediction);
+  const box = document.getElementById('box');
+                // Cambiar el color del recuadro según el sentimiento
+                if (prediction.score > 0.5) {
+                    box.style.backgroundColor = '#8BC34A'; // Verde si es positivo
+                } else if (prediction.score < 0.5) {
+                    box.style.backgroundColor = '#FF5722'; // Naranja si es negativo
+                }
 }
