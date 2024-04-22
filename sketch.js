56041f29510dae3e9c28ec480b076b104bd07ea7
Javier Ortega Zepeda <j.ortega11@ufromail.cl>
2024-04-22 15:25:46 -0400

    deteccion funcional

--- sketch.js
+++ sketch.js
@@ -8,9 +8,10 @@ async function setup() {
   sentiment = ml5.sentiment('movieReviews', modelReady);
 }
 
- function modelReady() {
+async function modelReady() {
   console.log('Model Loaded!');
-  
+  const container = document.getElementById('comments-container');
+
   // const text = "I love this movie!";
   // prediction = sentiment.predict(text);
   // console.log(prediction);
@@ -20,29 +21,49 @@ async function setup() {
   //                   box.style.backgroundColor = '#8BC34A'; // Verde si es positivo
   //               } else if (prediction.score < 0.5) {
   //                   box.style.backgroundColor = '#FF5722'; // Naranja si es negativo
-   //               }
-   
+  //               }
+
   for (const comment of comments) {
     prediction = sentiment.predict(comment);
     clasifiedComments.push({
       text: comment,
       prediction: prediction.score
     });
-   }
-   
-   console.log(clasifiedComments);
+  }
+
+  console.log(clasifiedComments);
+  await createBoxes(clasifiedComments, container);
+
+
+}
+async function createBoxes(classifiedComments, container) {
+  for (const classifiedComment of classifiedComments) {
+    const div = document.createElement('div');
+    div.textContent = classifiedComment.text;
+    div.classList.add('box');
+
+    if (classifiedComment.prediction > 0.5) {
+      //div.style.backgroundColor = '#8BC34A';
+      div.classList.add('hover-effect');
+    } else if (classifiedComment.prediction < 0.5) {
+      //div.style.backgroundColor = '#FF5722';
+      div.classList.add('hover-effect-dos');
+    }
+
+    container.appendChild(div);
+  }
 }
 
-async function fetchComments () {
+async function fetchComments() {
   try {
-      const response = await fetch('https://dummyjson.com/comments');
-      const data = await response.json();
-      comments = data.comments;
-      commentBodies = comments.map(comment => comment.body);
-      return commentBodies;
+    const response = await fetch('https://dummyjson.com/comments');
+    const data = await response.json();
+    comments = data.comments;
+    commentBodies = comments.map(comment => comment.body);
+    return commentBodies;
   } catch (error) {
-      console.error('Error fetching data:', error);
-      throw error;
+    console.error('Error fetching data:', error);
+    throw error;
   }
 };
 
